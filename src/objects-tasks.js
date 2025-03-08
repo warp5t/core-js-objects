dc8b7daf4c2de506edeb463f80bb33a0668e6779
warp5t <dmitrij.didencko@yandex.by>
2025-03-08 13:36:54 +0300

    fix: fix error

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,7 +33,7 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(objects) {
+function mergeObjects(/* objects */) {
   throw new Error('Not implemented');
 }
 
