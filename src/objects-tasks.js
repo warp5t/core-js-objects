cef5cd800545a61351d24812944cccaafc64ce17
warp5t <dmitrij.didencko@yandex.by>
2025-03-10 13:24:56 +0300

    feat: solve task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,10 +33,15 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects.reduce((result, current) => {
+    const updatedResult = { ...result };
+    Object.entries(current).forEach(([key, value]) => {
+      updatedResult[key] = (updatedResult[key] || 0) + value;
+    });
+    return updatedResult;
+  }, {});
 }
-
 /**
  * Removes a properties from an object.
  *
